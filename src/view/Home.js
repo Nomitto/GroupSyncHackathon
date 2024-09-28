1531985ef28a0d34864c29d02001ac7ee666199d
Roshan Saigal <roshansaigal25@gmail.com>
2024-09-28 15:13:14 -0400

    merge resolving

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 import Popup from 'react'
 import { getGroups } from "../firebase";
-
-function Home() {
-    const logout = () => {
-        localStorage.clear()
-        window.location.reload()
-    }
-
-    return (
-        <div>
-            <h1>Home Page</h1>
-            <button onClick={logout}>Logout</button>
-        </div>
-    );
-=======
-import React, { useState } from "react";
 import CreateGroupButton from "../components/CreateGroupButton.tsx";
 import CreateGroupPopUp from "../components/CreateGroupPopUp.tsx";
 
@@ -48,7 +32,6 @@ function Home() {
       {showPopUp && <CreateGroupPopUp closePopUp={closePopUp} handleCreateGroup={handleCreateGroup} />}
     </div>
   );
->>>>>>> roshan
 }
 
 export default Home;
