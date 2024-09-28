28271ba75cd853c0515cf637869266dc057b7cc8
Sathwik Doddi <sathwik.doddi@gmail.com>
2024-09-28 15:47:56 -0400

    dynamic displays and more data fetching

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Popup from 'react'
-import { getGroups } from "../firebase";
+import { getGroups, getName, getUID, handleCreateGroup } from "../firebase";
 
 function Home() {
     const logout = () => {
@@ -8,9 +8,38 @@ function Home() {
         window.location.reload()
     }
 
+    const [userGroups, setUserGroups] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const res = await getGroups();
+                setUserGroups(res);
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchData()
+    }, []);
+
+    if (loading) {
+        return <p>Loading...</p>
+    }
+
     return (
         <div>
             <h1>Home Page</h1>
+            <ul>
+                {userGroups.map((item, index) => (
+                    <li key={index}>
+                        <p>{index}: {item}</p>
+                    </li>
+                ))}
+            </ul>
             <button onClick={logout}>Logout</button>
         </div>
     );
