46774866068dea1b5c3ed08aacebd0001c2f93e9
Roshan Saigal <roshansaigal25@gmail.com>
2024-09-28 14:41:28 -0400

    create group popup pops up, but no functionality

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import SignIn from './view/signin';
+import SignIn from './view/SignIn';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/view/Home.js
+++ src/view/Home.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import CreateGroupButton from "../components/CreateGroupButton.tsx";
+import CreateGroupPopUp from "../components/CreateGroupPopUp.tsx";
 
-function Home(){
-    const logout =()=>{
-        localStorage.clear()
-        window.location.reload()
-    }
-    return (
-        <div>
-            <h1>Home Page</h1>
-            <button onClick={logout}>Logout</button>
-        </div>
-    );
+function Home() {
+  const [showPopUp, setShowPopUp] = useState(false);
+
+  const openPopUp = () => {
+    setShowPopUp(true);
+  };
+
+  const handleCreateGroup = () => {
+    //TO be implemented
+  }
+
+  const closePopUp = () => {
+    setShowPopUp(false);
+  };
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <button onClick={logout}>Logout</button>
+      <CreateGroupButton onClick={openPopUp} />
+      {showPopUp && <CreateGroupPopUp closePopUp={closePopUp} />}
+    </div>
+  );
 }
+
 export default Home;
